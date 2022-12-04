61546341356bd42378184a7873703c994929a814
Lucy Veron <lucymveron@gmail.com>
2022-12-04 18:24:46 +0100

    Table component

--- employee-mgmt/src/App.tsx
+++ employee-mgmt/src/App.tsx
@@ -1,32 +1,23 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-
-export interface Employee {
-  id: number;
-  name: string;
-  status: string;
-}
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Employee } from "./Employee";
+import BasicTable from "./Table";
 
 function App() {
-
   const [employees, setEmployees] = useState<Employee[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:3001/employees')
-    .then((res: Response) => res.json())
-    .then((result: Employee[]) => {
-      setEmployees(result);
-    });
-  }, [])
+    fetch("http://localhost:3001/employees")
+      .then((res: Response) => res.json())
+      .then((result: Employee[]) => {
+        setEmployees(result);
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1>Employees</h1>
-      <ul>
-        {employees.map((employee: Employee) => (
-          <li key={employee.id}>{employee.name}: <strong>{employee.status}</strong></li>
-        ))}
-      </ul>
+      <BasicTable employees={employees} />
     </div>
   );
 }
